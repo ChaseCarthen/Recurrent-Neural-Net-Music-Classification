de1cbfdc8044a2ff215264a8b8235351d8383181
Chase Carthen <chasec2331@gmail.com>
2015-03-11 10:06:19 -0700

    Added rock

--- midiToBinaryVector.lua
+++ midiToBinaryVector.lua
@@ -31,11 +31,14 @@ output: spits out a torch float tensor.
   --print(binVector[note][i])
 end
 midiToBinaryVec = function(filename)
-    --print(filename)
+    print(filename)
     -- read the file
     local f = assert(io.open(filename, "r"))
     local t = f:read("*all")
-
+    if t == nil then
+    f:close()
+    return nil
+    end
     -- Set some local max and min variabes
     local min = 100000000
     local max = 0
